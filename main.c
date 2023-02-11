b58046b3264e5f24c35c3aaa4a54ab8e0ea9fbbd
dylan <dylanglandry@gmail.com>
2023-02-11 15:38:07 -0500

    dynamically allocate each card and arrays of cards
    This is so cards can move between the deck, discard pile, and each
    player's hand. Maybe not efficient, but it is how my mental model best
    understands the game, with cards actually moving from place to place.

--- main.c
+++ main.c
@@ -16,9 +16,10 @@ struct Card {
 	enum DrawEffect draw_effect;
 };
 
-void FillCards(struct Card *cards);
-void PrintCards(struct Card *cards);
-void ShuffleCards(struct Card *cards, int shuffles);
+struct Card **CreateCards();
+void FreeCards(struct Card **cards);
+void PrintCards(struct Card **cards);
+void ShuffleCards(struct Card **cards, int shuffles);
 
 char *ColorLabel(enum Color color);
 char *TurnEffectLabel(enum TurnEffect turn_effect);
@@ -27,57 +28,70 @@ char *DrawEffectLabel(enum DrawEffect draw_effect);
 int RandomInt(int min, int max);
 
 int main() {
-	struct Card cards[108];
-	FillCards(cards);
-	ShuffleCards(cards, 108);
+	struct Card **cards = CreateCards();
+	ShuffleCards(cards, 500);
 	PrintCards(cards);
+	FreeCards(cards);
 	return 0;
 }
 
 /**
- * Cards should be an array of 108 cards.
+ * Returns an array of 108 cards.
  */
-void FillCards(struct Card *cards) {
+struct Card **CreateCards() {
 	// An UNO deck has 108 cards in total.
+	struct Card **cards = malloc((sizeof(struct Card *)) * 108);
 	for (int i = 0; i < 108; i++) {
-		// Zero struct
-		cards[i].number = noNumber;
-		cards[i].color = noColor;
-		cards[i].turn_effect = noTurnEffect;
-		cards[i].draw_effect = noDrawEffect;
+		cards[i] = malloc(sizeof(struct Card));
+		cards[i]->number = noNumber;
+		cards[i]->color = noColor;
+		cards[i]->turn_effect = noTurnEffect;
+		cards[i]->draw_effect = noDrawEffect;
 
 		if (i < 76) {
-			// Cards 1-76: 76 colored & numbered cards There are 19 of each color.
-			// These are numbered 0-9, with each color having one 0 and two of 1-9.
-			cards[i].number = ceilf((i % 19) / 2.0);
-			cards[i].color = i / 19;
+			// Cards 1-76: 76 colored & numbered cards There are 19 of each color->
+			// These are numbered 0-9, with each color having one 0 and two of 1-9->
+			cards[i]->number = ceilf((i % 19) / 2.0);
+			cards[i]->color = i / 19;
 		} else if (i < 84) {
 			// Cards 77-84: 8 colored skip cards
-			cards[i].turn_effect = skip;
-			cards[i].color = (i - 76) / 2;
+			cards[i]->turn_effect = skip;
+			cards[i]->color = (i - 76) / 2;
 		} else if (i < 92) {
 			// Cards 85-92: 8 colored reverse cards
-			cards[i].turn_effect = reverse;
-			cards[i].color = (i - 84) / 2;
+			cards[i]->turn_effect = reverse;
+			cards[i]->color = (i - 84) / 2;
 		} else if (i < 100) {
 			// Cards 93-100: 8 colored draw 2 cards
-			cards[i].draw_effect = draw2;
-			cards[i].color = (i - 92) / 2;
+			cards[i]->draw_effect = draw2;
+			cards[i]->color = (i - 92) / 2;
 		} else if (i < 104) {
 			// Cards 101-104: 4 wild cards
-			cards[i].color = anyColor;
+			cards[i]->color = anyColor;
 		} else if (i < 108) {
 			// Cards 105-108: 4 wild draw 4 cards
-			cards[i].color = anyColor;
-			cards[i].draw_effect = draw4;
+			cards[i]->color = anyColor;
+			cards[i]->draw_effect = draw4;
 		}
 	}
+
+	return cards;
 }
 
-void PrintCards(struct Card *cards) {
+/**
+ * Frees an array of 108 cards.
+ */
+void FreeCards(struct Card **cards) {
+	for (int i = 0; i < 108; i++) {
+		free(cards[i]);
+	}
+	free(cards);
+};
+
+void PrintCards(struct Card **cards) {
 	printf("Here's what we have in our cards...\n");
 	for (int i = 0; i < 108; i++) {
-		struct Card *card = &cards[i];
+		struct Card *card = cards[i];
 		printf("Card #%i: ", i+1);
 		if (card->color != noColor) {
 			printf("%s ", ColorLabel(card->color));
@@ -95,12 +109,12 @@ void PrintCards(struct Card *cards) {
 	}
 }
 
-void ShuffleCards(struct Card *cards, int shuffles) {
+void ShuffleCards(struct Card **cards, int shuffles) {
 	for (int shuffle = 0; shuffle < shuffles; shuffle++) {
 		int card_a_position = RandomInt(0, 107);
 		int card_b_position = RandomInt(0,107);
-		struct Card card_a = cards[card_a_position];
-		struct Card card_b = cards[card_b_position];
+		struct Card *card_a = cards[card_a_position];
+		struct Card *card_b = cards[card_b_position];
 		cards[card_a_position] = card_b;
 		cards[card_b_position] = card_a;
 	}
