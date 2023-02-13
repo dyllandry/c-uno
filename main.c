b26f605a9111072b89adea7fd859c734fd1caff0
dylan <dylanglandry@gmail.com>
2023-02-12 20:30:35 -0500

    model deck struct & deck functions
    Put array of 108 allocated cards in a deck struct. This sort of
    formalizes the idea that a deck has 108 cards. Anything that receives a
    deck knows it has space for no more than 108 cards. This is better than
    functions receiving just an array of cards and presuming it has a length
    of 108.

--- main.c
+++ main.c
@@ -16,10 +16,13 @@ struct Card {
 	enum DrawEffect draw_effect;
 };
 
-struct Card **CreateCards();
-void FreeCards(struct Card **cards);
-void PrintCards(struct Card **cards);
-void ShuffleCards(struct Card **cards, int shuffles);
+struct Deck {
+	struct Card *cards[108];
+};
+
+void FillDeck(struct Deck *deck);
+void PrintDeck(struct Deck *deck);
+void ShuffleDeck(struct Deck *deck, int shuffles);
 
 char *ColorLabel(enum Color color);
 char *TurnEffectLabel(enum TurnEffect turn_effect);
@@ -28,70 +31,58 @@ char *DrawEffectLabel(enum DrawEffect draw_effect);
 int RandomInt(int min, int max);
 
 int main() {
-	struct Card **cards = CreateCards();
-	ShuffleCards(cards, 500);
-	PrintCards(cards);
-	FreeCards(cards);
+	struct Deck deck;
+	FillDeck(&deck);
+	ShuffleDeck(&deck, 500);
+	PrintDeck(&deck);
 	return 0;
 }
 
 /**
- * Returns an array of 108 cards.
+ * Creates 108 cards and fills an array with them.
  */
-struct Card **CreateCards() {
+void FillDeck(struct Deck *deck) {
 	// An UNO deck has 108 cards in total.
-	struct Card **cards = malloc((sizeof(*cards)) * 108);
 	for (int i = 0; i < 108; i++) {
-		cards[i] = malloc(sizeof(struct Card));
-		cards[i]->number = noNumber;
-		cards[i]->color = noColor;
-		cards[i]->turn_effect = noTurnEffect;
-		cards[i]->draw_effect = noDrawEffect;
+		struct Card *new_card = malloc(sizeof(struct Card));
+		new_card->number = noNumber;
+		new_card->color = noColor;
+		new_card->turn_effect = noTurnEffect;
+		new_card->draw_effect = noDrawEffect;
 
 		if (i < 76) {
 			// Cards 1-76: 76 colored & numbered cards There are 19 of each color->
 			// These are numbered 0-9, with each color having one 0 and two of 1-9->
-			cards[i]->number = ceilf((i % 19) / 2.0);
-			cards[i]->color = i / 19;
+			new_card->number = ceilf((i % 19) / 2.0);
+			new_card->color = i / 19;
 		} else if (i < 84) {
 			// Cards 77-84: 8 colored skip cards
-			cards[i]->turn_effect = skip;
-			cards[i]->color = (i - 76) / 2;
+			new_card->turn_effect = skip;
+			new_card->color = (i - 76) / 2;
 		} else if (i < 92) {
 			// Cards 85-92: 8 colored reverse cards
-			cards[i]->turn_effect = reverse;
-			cards[i]->color = (i - 84) / 2;
+			new_card->turn_effect = reverse;
+			new_card->color = (i - 84) / 2;
 		} else if (i < 100) {
 			// Cards 93-100: 8 colored draw 2 cards
-			cards[i]->draw_effect = draw2;
-			cards[i]->color = (i - 92) / 2;
+			new_card->draw_effect = draw2;
+			new_card->color = (i - 92) / 2;
 		} else if (i < 104) {
 			// Cards 101-104: 4 wild cards
-			cards[i]->color = anyColor;
+			new_card->color = anyColor;
 		} else if (i < 108) {
 			// Cards 105-108: 4 wild draw 4 cards
-			cards[i]->color = anyColor;
-			cards[i]->draw_effect = draw4;
+			new_card->color = anyColor;
+			new_card->draw_effect = draw4;
 		}
+		deck->cards[i] = new_card;
 	}
-
-	return cards;
 }
 
-/**
- * Frees an array of 108 cards.
- */
-void FreeCards(struct Card **cards) {
-	for (int i = 0; i < 108; i++) {
-		free(cards[i]);
-	}
-	free(cards);
-};
-
-void PrintCards(struct Card **cards) {
+void PrintDeck(struct Deck *deck) {
 	printf("Here's what we have in our cards...\n");
 	for (int i = 0; i < 108; i++) {
-		struct Card *card = cards[i];
+		struct Card *card = deck->cards[i];
 		printf("Card #%i: ", i+1);
 		if (card->color != noColor) {
 			printf("%s ", ColorLabel(card->color));
@@ -109,14 +100,14 @@ void PrintCards(struct Card **cards) {
 	}
 }
 
-void ShuffleCards(struct Card **cards, int shuffles) {
+void ShuffleDeck(struct Deck *deck, int shuffles) {
 	for (int shuffle = 0; shuffle < shuffles; shuffle++) {
 		int card_a_position = RandomInt(0, 107);
 		int card_b_position = RandomInt(0,107);
-		struct Card *card_a = cards[card_a_position];
-		struct Card *card_b = cards[card_b_position];
-		cards[card_a_position] = card_b;
-		cards[card_b_position] = card_a;
+		struct Card *card_a = deck->cards[card_a_position];
+		struct Card *card_b = deck->cards[card_b_position];
+		deck->cards[card_a_position] = card_b;
+		deck->cards[card_b_position] = card_a;
 	}
 };
 
--- readme.md
+++ readme.md
@@ -4,8 +4,6 @@ Uno made with C!
 
 ## Todo
 
-- model the deck
-	- Has an array of dynamically allocated cards with a static length of 108. Since deck starts with 108 cards and will never have any more. Don't care about shrinking as cards are drawn.
 - model player's hands
 	- Has a dynamically allocated array of dynamically allocated cards so the array can grow in size. Players might have only 5 cards in their hand, and possibly never more than 15.
 - model the discard pile
@@ -15,8 +13,9 @@ Uno made with C!
 
 ## Todo History
 
-- *2023-02-11* shuffling
-- *2023-02-10* model card data by card effects
-- *2023-02-07* add FillCards() and PrintCards()
-- *2023-02-06* start project
+- *2023-02-12*: model the deck
+- *2023-02-11*: shuffling
+- *2023-02-10*: model card data by card effects
+- *2023-02-07*: add FillCards() and PrintCards()
+- *2023-02-06*: start project
 
