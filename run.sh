48c4c572ee4e8b3e75ba3cc2270983e05e8c4e37
Dylan Landry <dylan.landry@milkmoovement.com>
2023-03-10 17:58:09 -0500

    silent mkdir if directory exists

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir out
+mkdir -p out
 gcc -std=c99 main.c card.c array.c -lm -o out/main.out && ./out/main.out
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir out
+mkdir -p out
 gcc -std=c99 test.c card.c array.c -lm -o out/test.out && ./out/test.out
 
